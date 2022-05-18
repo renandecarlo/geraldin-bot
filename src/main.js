70c02f0b6716b0b28d87e13e6afc1146ff3cc690
renandecarlo <renan.decarlo@yahoo.com.br>
2022-05-18 03:45:20 -0300

    Keep console window running

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const auth = require('./auth');
+const readline = require('readline');
 
 (async () => {    
     if(await auth.signin()) {
         require('./whatsapp-bot');
         require('./geraldo-listener');
     }
-})()
+})();
+
+/* Keep console running */
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
