5c1725fbeb6bc3af6557c3f00f84b2f6c7e5dbde
Renan D <renan.decarlo@yahoo.com.br>
2022-07-08 14:33:50 -0300

    Disable venom auto close for QR scan

--- src/whatsapp-bot.js
+++ src/whatsapp-bot.js
@@ -11,7 +11,7 @@ const config = {
 
 /* Start venom browser */
 venom
-	.create('geraldo-bot', false, handleSession, { headless: config.headless, multidevice: true })
+	.create('geraldo-bot', false, handleSession, { headless: config.headless, multidevice: true, autoClose: false })
 	.then((client) => start(client))
 	.catch((erro) => {
 		console.log(erro);
