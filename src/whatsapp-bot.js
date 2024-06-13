bf0b5af7e2bd8f13cf36d68cab6726eacade8437
Renan D. <renan.decarlo@yahoo.com.br>
2024-06-13 15:28:07 -0300

    Remove fixed wpp version

--- src/whatsapp-bot.js
+++ src/whatsapp-bot.js
@@ -177,7 +177,7 @@ let client;
 			autoClose: false, 
 			deviceSyncTimeout: false,
 			browserArgs: ['--disable-extensions'],
-			whatsappVersion: '2.3000.1014090025-alpha',
+			// whatsappVersion: null
 		});
 
 		client.page.on('close', () => {
