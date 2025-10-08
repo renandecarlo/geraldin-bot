143f1cb280a30929cb2e1c08131548c9a2a12e96
Renan Decarlo <renan.decarlo@yahoo.com.br>
2025-10-08 00:03:14 -0300

    Bump whatsapp fixed version

--- src/whatsapp-bot.js
+++ src/whatsapp-bot.js
@@ -178,7 +178,7 @@ let client;
 			deviceSyncTimeout: false,
 			browserArgs: ['--disable-extensions'],
 			waitForLogin: true,
-			whatsappVersion: '2.3000.10258x'
+			whatsappVersion: '2.3000.10271x'
 		});
 
 		client.page.on('close', () => {
